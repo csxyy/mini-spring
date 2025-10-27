b36025950c95b291768911be7143061ea38d8065
csx <17771649720@163.com>
2025-10-28 00:05:41 +0800

    自动注入

---
+++ mini-spring-beans/src/main/java/com/spring/beans/factory/annotation/AutowiredAnnotationBeanPostProcessor.java
@@ -0,0 +1,12 @@
+package com.spring.beans.factory.annotation;
+
+/**
+ * ClassName: AutowiredAnnotationBeanPostProcessor
+ * Description:
+ *
+ * @Author: csx
+ * @Create: 2025/10/25 - 1:42
+ * @version: v1.0
+ */
+public class AutowiredAnnotationBeanPostProcessor {
+}
